6988691ab6b806bb669e46c3a1e284e85691b0b6
ValeriiKrykun <krykun92@gmail.com>
2020-09-12 16:27:04 +0300

    fix gitignore

---
+++ IdentityToDoList/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using IdentityToDoList.Entities;
+using IdentityToDoList.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityToDoList.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<TodoListData> TodoListData { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TodoListData>()
+                .HasOne(e => e.ApplicationUsers)
+                .WithMany(c => c.TodoListData);
+            base.OnModelCreating(builder);
+        }
+    }
+}
